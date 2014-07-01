e9c920c7e5244131c7dd63ccdda1beed74ddc480
sejour <develop.sejour@gmail.com>
2014-07-01 19:41:35 +0900

    データが一つも存在しない時にデキューされた際はnilを返すように修正。

--- HomeWorks/1.1/BasicObjectiveC/BasicObjectiveC/TestQueue.m
+++ HomeWorks/1.1/BasicObjectiveC/BasicObjectiveC/TestQueue.m
@@ -37,6 +37,11 @@
 /** デキュー **/
 - (id)dequeue
 {
+    // データが一つも存在しなければnilを返す
+    if (data.count < 1) {
+        return nil;
+    }
+    
     id first = data.firstObject;
     
     [data removeObjectAtIndex:0];
